62680c05aa1ceb515c631270b41a9ec09f1967ed
sagarPakhrin <sagarlama826@gmail.com>
2023-05-26 00:03:13 +0545

    add seo tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="description"
+          content="Sagar Lama is a Software Engineer at Avesha.io"
+        />
+        <meta name="author" content="Sagar Lama" />
+        <meta name="twitter:creator" content="@sagarllp" />
+        <meta name="twitter:url" content="https://twitter.com/sagarllp" />
+        <meta name="twitter:site" content="@sagarllp" />
+        <meta
+          name="twitter:description"
+          content="Sagar Lama is a Software Engineer at Avesha.io"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Sagar Lama" />
+
+        <meta property="og:image" content="" />
+        <meta name="twitter:image" content="" />
+        <meta name="twitter:image:src" content="" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:title" content="" />
+        <meta
+          property="og:description"
+          content="Sagar Lama is a Software Engineer at Avesha.io"
+        />
+      </head>
       <body>
         <Header />
         {children}
