cb4a4efc5549446440f04a73606d80602f7acb2c
sagarPakhrin <sagarlama826@gmail.com>
2023-06-23 23:25:12 +0545

    feat(): Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://sagarlama.com',
+  generateRobotsTxt: true, // (optional)
+  priority: 0.9,
+  generateIndexSitemap: false,
+  exclude: ['/favicon.ico', '/api/*'],
+};
