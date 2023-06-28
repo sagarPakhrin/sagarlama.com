e690c93a6fa58485273b9cf6c7373908b23d65b5
sagarPakhrin <sagarlama826@gmail.com>
2023-06-29 00:15:14 +0545

    fix(): Generate meta description

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -10,7 +10,7 @@ export const generateStaticParams = async () =>
 export const generateMetadata = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
   if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
-  return { title: post.title };
+  return { title: post.title, description: post.description };
 };
 
 const getBlogFromSlug = async (slug: string) => {
