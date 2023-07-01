53cbc8c9061116e1e1e557513ca8e7eaf82ceb73
sagarPakhrin <sagarlama826@gmail.com>
2023-07-01 14:52:11 +0545

    fix(): Uncoment genearete meta

--- src/app/post/[year]/[month]/[slug]/page.tsx
+++ src/app/post/[year]/[month]/[slug]/page.tsx
@@ -7,22 +7,22 @@ import { notFound } from 'next/navigation';
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
 
-// export const generateMetadata = ({ params }: { params: { slug: string } }) => {
-//   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
-//   if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
-//   return {
-//     title: post.title,
-//     description: post.description,
-//     openGraph: {
-//       title: post.title,
-//       description: post.description,
-//       // 'twitter:title': post.title,
-//     },
-//     twitter: {
-//       title: post.title,
-//     },
-//   };
-// };
+export const generateMetadata = ({ params }: { params: { slug: string } }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
+  return {
+    title: post.title,
+    description: post.description,
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      // 'twitter:title': post.title,
+    },
+    twitter: {
+      title: post.title,
+    },
+  };
+};
 
 const getBlogFromSlug = async (slug: string) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === slug);
