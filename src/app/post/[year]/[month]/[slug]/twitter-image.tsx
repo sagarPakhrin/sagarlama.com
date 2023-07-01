3cf6242c0bab732964fb452fd1344dcaa811b36f
sagarPakhrin <sagarlama826@gmail.com>
2023-07-02 00:04:22 +0545

    fixup! fix(): Fix meta image

--- src/app/post/[year]/[month]/[slug]/twitter-image.tsx
+++ src/app/post/[year]/[month]/[slug]/twitter-image.tsx
@@ -1,6 +1,8 @@
 import { ImageResponse } from 'next/server';
 import { allPosts } from 'contentlayer/generated';
 import { PostLayoutProps } from './page';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
 export const runtime = 'edge';
 
@@ -11,25 +13,30 @@ export const size = {
 };
 export const contentType = 'image/png';
 
-export default async function Image({ params }: { params: PostLayoutProps }) {
+export default async function Og({ params }: { params: PostLayoutProps }) {
   const slug = `${params.year}/${params.month}/${params.slug}`;
   const post = allPosts.find((post) => post._raw.flattenedPath === slug);
 
+  if (!post) notFound();
+
   return new ImageResponse(
     (
-      <div
-        style={{
-          fontSize: 48,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        {post!.title}
-      </div>
+      <>
+        {post.cover_image ? (
+          <div tw="h-full w-full">
+            <Image src={post.cover_image} alt={alt} fill />
+          </div>
+        ) : (
+          <div tw="h-full w-full pb-10">
+            <div tw="flex flex-col text-neutral-50">
+              <div tw="text-7xl font-bold">{post?.title}</div>
+              <div tw="flex mt-6 flex-wrap items-center text-4xl text-neutral-200 truncate">
+                {post.description}
+              </div>
+            </div>
+          </div>
+        )}
+      </>
     ),
     {
       ...size,
