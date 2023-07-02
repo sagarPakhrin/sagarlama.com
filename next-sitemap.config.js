f73ca4bf5a6b2868930759fa0e03eaab15a183a7
sagarPakhrin <sagarlama826@gmail.com>
2023-07-02 13:38:56 +0545

    fix(): Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://sagarlama.com',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://www.sagarlama.com',
   generateRobotsTxt: true, // (optional)
   priority: 0.9,
   generateIndexSitemap: false,
